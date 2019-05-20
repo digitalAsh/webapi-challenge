4ab55e34dfbd20d3c983cc19af2c8e0d7f118ec0
jal9626 <jal9626@gmail.com>
2019-05-19 22:11:55 -0400

    delete endpoint added to projectRouter

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -38,4 +38,20 @@ router.put('/:id', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const count = await Projects.remove(req.params.id)
+        if (count > 0) {
+            res.status(200).json({ message: 'The project has been nuked' });
+        } else {
+            res.status(404).json({ message: 'The project could not be found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error removing post'
+        });
+    }
+});
+
 module.exports = router;
