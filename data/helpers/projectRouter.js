f4f361d70ecb42135305d1c027c315292e6ba206
jal9626 <jal9626@gmail.com>
2019-05-19 22:02:07 -0400

    added get and put endpoints to projectRouter

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -9,4 +9,33 @@ router.use((req, res, next) => {
     next();
   })
 
+//url /api/projects  
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Projects.get(req.params.id);
+        res.status(200).json(projects); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error retrieving the posts'
+        });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const project = await Projects.update(req.params.id, req.body);
+        if (project) {
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ message:'The project could not be found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error updating project'
+        });
+    }
+});
+
 module.exports = router;
